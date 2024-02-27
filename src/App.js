63d3728d5d35c4668bd42e8a47108af17166c583
linnaijun <g111234004@grad.ntue.edu.tw>
2024-02-27 10:03:41 +0800

    優化位置

--- src/App.js
+++ src/App.js
@@ -16,11 +16,18 @@ function App() {
   const [sidebarText, setSidebarText] = useState('初始文字');
 
   const handleScroll = () => {
+    // 获取视窗高度的一半
+    const halfWindowHeight = window.innerHeight / 2;
+    // 计算当前滚动位置加上半个视窗的高度
+    const scrollPosition = window.pageYOffset + halfWindowHeight;
+  
+    // 获取每个区块顶部的位置
     const titlePosition = document.getElementById('title').offsetTop;
     const introductionPosition = document.getElementById('introduction').offsetTop;
     const albumPosition = document.getElementById('album').offsetTop;
-    const scrollPosition = window.pageYOffset;
-
+  
+    // 计算下一个区块的开始位置作为当前区块的结束条件
+    // 注意：这里假设相册是最后一个区块，所以它的结束条件稍有不同
     if (scrollPosition >= titlePosition && scrollPosition < introductionPosition) {
       setSidebarText('標題');
     } else if (scrollPosition >= introductionPosition && scrollPosition < albumPosition) {
@@ -29,6 +36,7 @@ function App() {
       setSidebarText('相冊');
     }
   };
+  
 
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
