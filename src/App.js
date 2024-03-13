73be676e0573e20c5ac568f2023acea6f7181117
linnaijun <g111234004@grad.ntue.edu.tw>
2024-03-13 10:27:21 +0800

    左側欄位更新

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Title from './Title';
 import Introduction from './Introduction';
-import Album from './Album';
+import Camp from './camp';
 import React, { useEffect, useState } from 'react';
 import './App.css'; // 確保你的樣式被正確導入
 
@@ -25,7 +25,17 @@ function App() {
   const [currentText, setCurrentText] = useState('標題');
   const [newText, setNewText] = useState('標題');
   const [newTextAnimation, setNewTextAnimation] = useState('標題'); // 定义 newTextAnimation 状态及其更新函数
-
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+ 
+  const COLORS = {
+    oddSectionBackground: '#7EC0FC', // 原本的顏色
+    evenSectionBackground: '#004B8F', // 偶數頁面的背景色
+  };
+  const [sidebarBgColor, setSidebarBgColor] = useState(COLORS.oddSectionBackground); // 初始背景色
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+  
   useEffect(() => {
     document.title = "網頁標題";
   }, []); // 空依賴數組表示這個效果只會在組件掛載時運行一次
@@ -56,23 +66,26 @@ function App() {
     const halfWindowHeight = window.innerHeight / 2;
     // 计算当前滚动位置加上半个视窗的高度
     const scrollPosition = window.pageYOffset + halfWindowHeight;
-
+  
     // 获取每个区块顶部的位置
     const titlePosition = document.getElementById('title').offsetTop;
     const introductionPosition = document.getElementById('introduction').offsetTop;
-    const albumPosition = document.getElementById('album').offsetTop;
-
-    // 计算下一个区块的开始位置作为当前区块的结束条件
-    // 注意：这里假设相册是最后一个区块，所以它的结束条件稍有不同
+    const campPosition = document.getElementById('camp').offsetTop;
+  
+    // 根据滚动位置更新文本和背景色
     if (scrollPosition >= titlePosition && scrollPosition < introductionPosition) {
       setNewText('標題');
-    } else if (scrollPosition >= introductionPosition && scrollPosition < albumPosition) {
+      setSidebarBgColor(COLORS.oddSectionBackground);
+    } else if (scrollPosition >= introductionPosition && scrollPosition < campPosition) {
       setNewText('介紹');
-    } else if (scrollPosition >= albumPosition) {
+      setSidebarBgColor(COLORS.evenSectionBackground);
+    } else if (scrollPosition >= campPosition) {
       setNewText('相冊');
+      // 假设相册页面视为奇数页面
+      setSidebarBgColor(COLORS.oddSectionBackground);
     }
   };
-
+  
 
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
@@ -85,17 +98,19 @@ function App() {
 
     <div className="App">
     <div className="header">
-      <button className="linkButton" onClick={() => scrollToSection('title')}>標題</button>
-      <button className="linkButton" onClick={() => scrollToSection('introduction')}>介紹</button>
-      <button className="linkButton" onClick={() => scrollToSection('album')}>相冊</button>
-    </div>
+    <button className="menuButton" onClick={toggleMenu}>Menu</button>
+  <div className={`links ${isMenuOpen ? 'show' : ''}`}>
+    <button className="linkButton" onClick={() => scrollToSection('title')}>標題</button>
+    <button className="linkButton" onClick={() => scrollToSection('introduction')}>介紹</button>
+    <button className="linkButton" onClick={() => scrollToSection('camp')}>相冊</button>
+  </div> </div>
     <div className="content" style={{paddingTop: '60px'}}> {/* 添加顶部留白以避免内容被标题栏遮挡 */}
-      <div className="fixedBox">
+    <div className="fixedBox" style={{ backgroundColor: sidebarBgColor }}>
         <div className={`verticalText ${newTextAnimation}`}>{currentText}</div>
       </div>
       <Title height="1000px" id="title"/>
       <Introduction height="2000px" id="introduction"/>
-      <Album height="3000px" id="album"/>
+      <Camp height="3000px" id="camp"/>
       <button onClick={scrollToTop} className="scrollToTopButton"></button>
     </div>
   </div>
