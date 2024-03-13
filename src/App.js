b890e6980385d2007dc259f1b6c132a698ea7151
linnaijun <g111234004@grad.ntue.edu.tw>
2024-03-13 10:47:45 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -62,37 +62,25 @@ function App() {
 
 
   const handleScroll = () => {
-    // 获取视窗高度的一半
     const halfWindowHeight = window.innerHeight / 2;
-    // 计算当前滚动位置加上半个视窗的高度
     const scrollPosition = window.pageYOffset + halfWindowHeight;
   
-    // 获取每个区块顶部的位置
     const titlePosition = document.getElementById('title').offsetTop;
     const introductionPosition = document.getElementById('introduction').offsetTop;
     const campPosition = document.getElementById('camp').offsetTop;
-
   
-    // 根据滚动位置更新文本和背景色
     if (scrollPosition >= titlePosition && scrollPosition < introductionPosition) {
       setNewText('標題');
       setSidebarBgColor(COLORS.oddSectionBackground);
     } else if (scrollPosition >= introductionPosition && scrollPosition < campPosition) {
       setNewText('介紹');
       setSidebarBgColor(COLORS.evenSectionBackground);
-    // 计算下一个区块的开始位置作为当前区块的结束条件
-    // 注意：这里假设相册是最后一个区块，所以它的结束条件稍有不同
-    if (scrollPosition >= titlePosition && scrollPosition < introductionPosition) {
-      setNewText('標題');
-    } else if (scrollPosition >= introductionPosition && scrollPosition < campPosition) {
-      setNewText('介紹');
-
     } else if (scrollPosition >= campPosition) {
       setNewText('相冊');
-      // 假设相册页面视为奇数页面
       setSidebarBgColor(COLORS.oddSectionBackground);
     }
   };
+  
   
 
   useEffect(() => {
@@ -103,33 +91,27 @@ function App() {
   }, []);
 
   return (
-
     <div className="App">
-    <div className="header">
-    <button className="menuButton" onClick={toggleMenu}>Menu</button>
-  <div className={`links ${isMenuOpen ? 'show' : ''}`}>
-    <button className="linkButton" onClick={() => scrollToSection('title')}>標題</button>
-    <button className="linkButton" onClick={() => scrollToSection('introduction')}>介紹</button>
-    <button className="linkButton" onClick={() => scrollToSection('camp')}>相冊</button>
-  </div> </div>
-
-      <button className="linkButton" onClick={() => scrollToSection('title')}>標題</button>
-      <button className="linkButton" onClick={() => scrollToSection('introduction')}>介紹</button>
-      <button className="linkButton" onClick={() => scrollToSection('camp')}>相冊</button>
-    </div>
-
-    <div className="content" style={{paddingTop: '60px'}}> {/* 添加顶部留白以避免内容被标题栏遮挡 */}
-    <div className="fixedBox" style={{ backgroundColor: sidebarBgColor }}>
-        <div className={`verticalText ${newTextAnimation}`}>{currentText}</div>
+      <div className="header">
+        <button className="menuButton" onClick={toggleMenu}>Menu</button>
+        <div className={`links ${isMenuOpen ? 'show' : ''}`}>
+          <button className="linkButton" onClick={() => scrollToSection('title')}>標題</button>
+          <button className="linkButton" onClick={() => scrollToSection('introduction')}>介紹</button>
+          <button className="linkButton" onClick={() => scrollToSection('camp')}>相冊</button>
+        </div>
       </div>
-      <Title height="1000px" id="title"/>
-      <Introduction height="2000px" id="introduction"/>
-      <Camp height="3000px" id="camp"/>
-      <button onClick={scrollToTop} className="scrollToTopButton"></button>
+
+      <div className="content" style={{paddingTop: '60px'}}>
+        <div className="fixedBox" style={{ backgroundColor: sidebarBgColor }}>
+          <div className={`verticalText ${newTextAnimation}`}>{currentText}</div>
+        </div>
+        <Title height="1000px" id="title"/>
+        <Introduction height="2000px" id="introduction"/>
+        <Camp height="3000px" id="camp"/>
+        <button onClick={scrollToTop} className="scrollToTopButton"></button>
+      </div>
     </div>
-  </div>
   );
 }
-
 
 export default App;
